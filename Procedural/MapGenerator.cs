6fe007716a8c61fdb4c7ac43735b7ca536509112
SimantoR <md.simanto_rahman@yahoo.com>
2018-10-19 18:04:35 -0230

    Changed class structure of MapGenerator

--- Procedural/MapGenerator.cs
+++ Procedural/MapGenerator.cs
@@ -1,60 +1,60 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace PSYEngine.Unity.Procedural
 {
-    /// <summary>
-    /// Manages map generations
-    /// </summary>
-    public class MapGenerator
+    public sealed class MapGenerator : MonoBehaviour
     {
-        public NoiseConfiguration Configuration { get { return _config; } }
+        public int Width = 200;
 
-        private NoiseConfiguration _config { get; set; }
+        public int Height = 200;
 
-        /// <summary>
-        /// Create a new instance of <see cref="MapGenerator"/>
-        /// </summary>
-        /// <param name="configuration"><see cref="Procedural.NoiseConfiguration"/> configuration for Noisemap generation</param>
-        public MapGenerator(NoiseConfiguration configuration) => this._config = configuration;
+        [Tooltip("Number of sign waves to apply")]
+        public int Octaves = 3;
 
-        /// <summary>
-        /// Generates height map data
-        /// </summary>
-        /// <param name="seed">Seed to use for generation</param>
-        /// <param name="scale">Scale to apply on genareted noise</param>
-        /// <returns>Returns <see cref="Buffer2D{T}"/> array of height data</returns>
-        /// <exception cref="System.InsufficientMemoryException"/>
-        public float[,] GenerateNoiseMap(int seed, float scale)
+        public float Lacunarity = 2f;
+
+        public float Persistance = 0.5f;
+
+        public int Seed = 0;
+
+        public AnimationCurve HeightCurve;
+
+        public UnityEngine.Vector2 Offset = new Vector2(1, 1);
+
+        private float[,] heightMap;
+
+        public float[,] GenerateHeightMap(int seed, float scale)
         {
             if (scale <= 0)
                 scale = 0.0001f;
 
             System.Random random = new System.Random(seed);
 
-            Vector2[] octaveOffsets = new Vector2[_config.Octaves];
+            Vector2[] octaveOffsets = new Vector2[Octaves];
 
-            for (int i = 0; i < _config.Octaves; i++)
+            for (int i = 0; i < Octaves; i++)
             {
-                float offsetX = random.Next(-100000, 100000) + _config.Offset.x;
-                float offsetY = random.Next(-100000, 100000) + _config.Offset.y;
+                float offsetX = random.Next(-100000, 100000) + Offset.x;
+                float offsetY = random.Next(-100000, 100000) + Offset.y;
                 octaveOffsets[i].Set(offsetX, offsetY);
             }
 
-            float[,] noiseMap = new float[_config.Width, _config.Height];
+            float[,] noiseMap = new float[Width, Height];
 
             float maxNoiseHeight = float.MinValue;
             float minNoiseHeight = float.MaxValue;
 
-            Parallel.For(0, _config.Height, y =>
+            Parallel.For(0, Height, y =>
             {
-                for (ushort x = 0; x < _config.Width; x++)
+                for (ushort x = 0; x < Width; x++)
                 {
                     float amplitude = 1;
                     float frequency = 1;
                     float noiseHeight = 0;
 
-                    for (ushort i = 0; i < _config.Octaves; i++)
+                    for (ushort i = 0; i < Octaves; i++)
                     {
                         // Control frequency of octaves
                         float sampleX = x / scale * frequency + octaveOffsets[i].x;
@@ -66,10 +66,10 @@ namespace PSYEngine.Unity.Procedural
                         noiseHeight += perlinValue * amplitude;
 
                         // Amplitude = Persistance ^ i
-                        amplitude *= _config.Persistance;
+                        amplitude *= Persistance;
 
                         // Frequency = Lacunarity ^ i
-                        frequency *= _config.Lacunarity;
+                        frequency *= Lacunarity;
                     }
 
                     // Determine max height
